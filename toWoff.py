d36c273df10c9bfbaf4d9c2280a961c39fc05018
hanwm <hanwm@chsi.com.cn>
2023-11-01 11:03:03 +0800

    结构调整

--- toWoff.py
+++ toWoff.py
@@ -1,7 +1,7 @@
 from fontTools.ttLib import TTFont
-ttf_font = TTFont("zkhl.ttf")
+ttf_font = TTFont("./dist/zkhl.ttf")
 
 ttf_font.flavor = "woff"
-ttf_font.save("zkhl.woff")
+ttf_font.save("./dist/zkhl.woff")
 ttf_font.flavor = "woff2"
-ttf_font.save("zkhl.woff2")
+ttf_font.save("./dist/zkhl.woff2")
